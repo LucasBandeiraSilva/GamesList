cda90242b74159664e141d78a9fb7f25d84d3c40
Lucas Bandeira <lucasbandeirai40@gmail.com>
2023-07-12 14:38:26 -0300

    GET game by id

--- src/main/java/com/lucasbandeira/dslist/controller/GameController.java
+++ src/main/java/com/lucasbandeira/dslist/controller/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucasbandeira.dslist.dto.GameDto;
 import com.lucasbandeira.dslist.dto.GameMinDto;
 import com.lucasbandeira.dslist.services.GameService;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDto> findAll() {
         List<GameMinDto> result = gameService.findAll();
+        return result;
+    }
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id) {
+        GameDto result = gameService.findById(id);
         return result;
     }
 }
